8bc082517f717fa1531ab6a95d6e925ebdb1ded3
ParkJongYoon <qkrwhddbs97@naver.com>
2025-01-02 17:20:49 +0900

    Adminmart

---
+++ Adminmart/Adminmart/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+using System.Diagnostics;
+using Adminmart.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Adminmart.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult TicketList()
+        {
+            
+            return View();
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
