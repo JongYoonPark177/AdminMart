f406b0b40bcd34c20be557ec1ce513b9d5464968
jongypark <91310693+jongypark@users.noreply.github.com>
2025-01-02 16:34:38 +0900

    Delete AdminMart directory

--- AdminMart/AdminMart/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AdminMart.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
